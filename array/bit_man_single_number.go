618f1f86e6cb856d53e7e905124dad643b8ead42
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-04 20:02:09 +0530

    feat : added plus one problem

--- array/bit_man_single_number.go
+++ array/bit_man_single_number.go
@@ -8,3 +8,9 @@ func SingleNumber(nums []int) int {
 
 	return res
 }
+
+/*
+The XOR operation has two important properties:
+ -> Self-Canceling: For any integer a, a⊕a=0. This means that if you XOR a number with itself, the result is zero.
+ -> Identity with Zero: For any integer a, a⊕0=a. This means that XORing any number with zero returns that number.
+*/
