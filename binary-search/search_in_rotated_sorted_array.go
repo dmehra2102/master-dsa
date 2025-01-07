9f6406d10c1843bd0266f597ee258a6427dc4845
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-07 11:22:51 +0530

    feat : added binary search problems

---
+++ binary-search/search_in_rotated_sorted_array.go
@@ -0,0 +1,27 @@
+package binarysearch
+
+func Search(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[start] <= nums[mid] {
+			if nums[start] <= target && target < nums[mid] {
+				end = mid - 1
+			} else {
+				start = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[end] {
+				start = mid + 1
+			} else {
+				end = mid - 1
+			}
+		}
+	}
+	return -1
+}
